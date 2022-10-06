14dca22a41149c5041b50f9c29d08c18b7093e1d
Laila Dyer <laila.annika.dyer@gmail.com>
2022-10-06 14:01:19 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+
+namespace GamePlan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var settings = MongoClientSettings.FromConnectionString(ConnectionString.getConnectionString());
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var client = new MongoClient(settings);
+            var dbList = client.ListDatabases().ToList();
+
+            foreach (var db in dbList)
+            {
+                textBoxTest.Text += db + "\n";
+            }
+        }
+    }
+}
